6456406ca51d9e36abf3cf8fced2f6467c24a5da
Michael Adair <mjadair91@gmail.com>
2020-04-27 17:55:44 +0100

    basic logic

--- FareCalculator.java
+++ FareCalculator.java
@@ -3,48 +3,97 @@ import java.lang.Math;
 
 class FareCalculator {
 
-int daysUsing;
-int numOfTrips;
-String ticketType;
-double fareCost;
+  int daysUsing;
+  int numOfTrips;
+  String[] fareTypes = { "Single journey", "7 day travelcard", "Monthly travelcard", "Annual travelcard" };
+  double[] farePrices = { 2.90, 27, 103.70, 1080 };
 
+  public FareCalculator(int days, int trips) {
+    this.daysUsing = days;
+    this.numOfTrips = trips;
+  }
 
+  public double sevenDayPrice() {
 
+    double weeksUsed = (Math.ceil(this.daysUsing / 7)) * this.farePrices[1];
 
-public FareCalculator(int days, int trips, String fareType, double farePrice ) {
-  this.daysUsing = days;
-  this.numOfTrips = trips;
-  this.ticketType = fareType;
-  this.fareCost = farePrice;
-}
+    if (weeksUsed == 0.0) {
+      return this.farePrices[1];
+    }
 
+    return weeksUsed;
+  }
 
-public double sevenDayPrice() {
+  public double monthPrice() {
 
-  double weeksUsed = (Math.ceil(this.daysUsing / 7)) * this.fareCost;
+    double monthsUsed = (Math.ceil(this.daysUsing / 30)) * this.farePrices[2];
 
-  return weeksUsed;
+    if (monthsUsed == 0.0) {
+      return this.farePrices[2];
+    }
 
+    return monthsUsed;
+  }
 
+  public double annualPrice() {
 
+    double yearsUsed = (Math.ceil(this.daysUsing / 365)) * this.farePrices[3];
 
-}
+    if (yearsUsed == 0.0) {
+      return this.farePrices[3];
+    }
 
+    return yearsUsed;
+  }
 
+  public double[] allFarePrices() {
 
-public static void main(String[] args) {
+    double[] allFarePrices = { this.numOfTrips * this.farePrices[0], this.sevenDayPrice(), this.monthPrice(),
+        this.annualPrice() };
 
-   String[] fareTypes =  {"Single journey", "7 day travelcard", "Monthly travelcard", "Annual travelcard"};
-   double[] farePrices = {2.90, 27, 103.70, 1080};
+    return allFarePrices;
 
+  }
 
-  FareCalculator firstTest = new FareCalculator(42, 65, fareTypes[1], farePrices[1]);
+  public String checkBestFare() {
 
-  System.out.println(firstTest.ticketType);
+    double[] bestFare = this.allFarePrices();
 
+    int index = 0;
+    double min = bestFare[index];
 
-  
-}
+    for (int i = 1; i < bestFare.length; i++) {
 
+      if (bestFare[i] < min) {
+        min = bestFare[i];
+        index = i;
+      }
+
+    }
+    return this.fareTypes[index];
+
+  }
+
+  public static void main(String[] args) {
+
+    FareCalculator oneDay = new FareCalculator(1, 1);
+    FareCalculator busyDay = new FareCalculator(1, 40);
+    FareCalculator busyWeek = new FareCalculator(7, 45);
+    FareCalculator busyEight = new FareCalculator(8, 55);
+    FareCalculator busyTwentyTwo = new FareCalculator(22, 100);
+    FareCalculator busyMonth = new FareCalculator(30, 300);
+    FareCalculator busyTwoMonths = new FareCalculator(60, 300);
+    FareCalculator busyYear = new FareCalculator(364, 1000);
+
+    System.out.println("The best fare for one trip in a day is " + oneDay.checkBestFare());
+    System.out.println("The best fare for a busy day is " + busyDay.checkBestFare());
+    System.out.println("The best fare for a busy week is " + busyWeek.checkBestFare());
+    System.out.println("The best fare for eight days is " + busyEight.checkBestFare());
+    System.out.println("The best fare for 22 days is " + busyTwentyTwo.checkBestFare());
+    System.out.println("The best fare for a busy month is " + busyMonth.checkBestFare());
+    System.out.println("The best fare for a busy two months is " + busyTwoMonths.checkBestFare());
+    System.out.println("The best fare for a busy year is " + busyYear.checkBestFare());
+
+  }
 
 }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # London-Fare-Calculator
+
+A very basic tool to help cement my understanding of the basics of Java. Initializing a FareCalculator object assesses the number of days the user is travelling and the number of journeys they intend to make and returns the best priced-option. 
